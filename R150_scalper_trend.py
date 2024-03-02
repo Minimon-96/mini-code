946ae14b555c3a290945a430b56354e233190f44
kkkkasdas <https://github.com/kkkkasdas/mini-code>
2024-03-03 06:32:08 +0900

    2024/03/03 oreder state commit

--- R150_scalper_trend.py
+++ R150_scalper_trend.py
@@ -10,6 +10,15 @@ import os
 ### 거래할 코인 symbol
 coin = "KRW-XRP" 
 
+### 시작금액
+start_money = 150000
+
+### 현금이 N% 남으면 마지막 지정가 매도주문
+last_sell_order = 10
+
+### 마지막 지정가 매도 주문 수익률
+profitPer = 1.06
+
 ### API 키 파일 참조
 with open("key.txt") as f:
     access_key, secret_key = [line.strip() for line in f.readlines()]
@@ -44,7 +53,7 @@ def log(level, *args):
     now = datetime.now()
     real_time = now.strftime('%Y-%m-%d %H:%M:%S')
 
-    if level not in ("TR", "DG"):   # 로그레벨 미지정시 에러
+    if level not in ("TR", "DG", "INFO"):   # 로그레벨 미지정시 에러
         logs = f"TR|{real_time}|Log Level Error|"
         Logger.info(logs)
         return 0
@@ -163,15 +172,18 @@ def GET_ORDER_INFO(ticker, *args):  # 주문 내역 리턴 (uuid & bid or ask & 
                 if ret[i]['side'] == 'ask' or 'bid':
                     res = ret[i]['uuid'] +"&"+ ret[i]['side'] +"&"+ ret[i]['price'] +"&"+ ret[i]['volume']
                     log("TR", "Success", res)
+    except IndexError as ie:
+        res = 2
+        log("TR", "Try Last Sell Order", ie)
     except Exception as e:
-            res = 0
-            log("TR", "Fail", e)
+        res = 0
+        log("TR", "Fail", e)
     return res  # 조회된 주문내역 중 가장 마지막(최근) 주문내역 리턴
 
 @log_function_call
 def GET_ORDER_STATE(uuid):   # 주문 상태 리턴 (오류:error / 대기:wait / 완료:done)
     try:
-        retn = fetch_data(lambda: upbit.get_order(uuid))
+        retn = fetch_data(lambda: upbit.get_order(uuid, state='wait'))
         if "error" in retn:
             log("TR", "Error", retn)
             res = 0
@@ -263,27 +275,26 @@ def ORDER_SELL_MARKET(ticker, *args):   # 시장가 매도 주문 결과 리턴 
     return res
     
 @log_function_call
-def ORDER_SELL_LIMIT(ticker, *args):    # 지정가 매도 주문 결과 리턴 (지정한 Minimum Cash 가격에 도달한 경우 진행)
+def ORDER_SELL_LIMIT(ticker, profit, *args):    # 지정가 매도 주문 결과 리턴 (지정한 Minimum Cash 가격에 도달한 경우 진행)
+    if profit < 1.01:
+        log("TR", "Check your profiePer Value", profit)
     try:
         vol = math.floor(upbit.get_balance(ticker))     # 매도 수량 지정(소수점 첫째 자리에서 내림계산)
-        buy_avg_price = math.floor(1.02 * GET_BUY_AVG(ticker))      # 평균 매수가를 매도 주문 가격으로 지정
+        buy_avg_price = math.floor(profit * GET_BUY_AVG(ticker))      # 평균 매수가를 매도 주문 가격으로 지정
         res = upbit.sell_limit_order(ticker, buy_avg_price, vol)    
         if 'error' in res:
-            log("TR","Error", ticker, buy_avg_price, res)
-            res = 0
+            log("TR","Error", ticker, profit, buy_avg_price, res)
             return res
-        log("TR", " Success", ticker, buy_avg_price,res)
+        log("TR", " Success", ticker, profit, buy_avg_price,res)
         res = 1
     except Exception as e:
-        log("TR", "Fail", ticker, buy_avg_price, e)
-        res = 0
+        log("TR", "Fail", ticker, profit, buy_avg_price, e)
+        res = e
     return res
 
 def run(chk_run):
     if chk_run == 0:
         ### 초기변수 설정
-        start_money = 150000
-        Last_order_limit = 10                               # 현금이 N% 남으면 마지막 지정가 매도주문
         rise_chk = 0
         chk_sell_order = 0
         chk_15m_timer = 0
@@ -329,43 +340,39 @@ def run(chk_run):
     time.sleep(1)
 
     while chk_run == 1:
+        ### 현재 보유 현금이 0원인 경우는 업비트와의 통신에 문제가 있다고 판단하여 10초뒤 재실행
         cur_cash = GET_CASH(coin)
-        if cur_cash == 0:   # 현재 보유 현금이 0원인 경우는 업비트와의 통신에 문제가 있다고 판단하여 10초뒤 재실행
+        if cur_cash == 0:
             log("DG","The balance is confirmed as $0.")
             time.sleep(10)
             continue
-        
-        if int(time.strftime("%M")) % 15 == 0:  # 매시 15분 마다 타이머 초기화
-            chk_15m_timer = 0
 
-        min_cash = round((cur_cash + (cur_coin * cur_price)) * Last_order_limit/100)  # 최초 금액 대비 (10%) 가 되면 매수중지
+        ### 보유 현금액이 min_cash 미만이 되면 일괄매도 진행
+        cur_cash = GET_CASH(coin)
+        if cur_cash < min_cash:     
+            log("DG","Cash on hand is too low.")
+
+        ### 현재 코인의 가격이 0원인 경우 업비트와의 통신에 문제가 있다고 판단 하여 10초뒤 재실행
+        cur_price = GET_CUR_PRICE(coin) 
+        if cur_price == 0:
+            log("DG","Current Price small than 1.")
+            time.sleep(10)
+            continue
+
+        cur_coin = GET_QUAN_COIN(coin) 
+        if cur_coin * cur_price <= one_tick:
+            sell_price = 0.0
+        
+        one_tick = calculate_tick_unit(cur_price)
+
+        if chk_15m_timer != 0:
+            if int(time.strftime("%M")) % 15 == 0:  # 매시 15분 마다 타이머 초기화
+                log("INFO","Check Timer reset")
+                chk_15m_timer = 0
+
+        min_cash = round((cur_cash + (cur_coin * cur_price)) * last_sell_order/100)  # 최초 금액 대비 (10%) 가 되면 매수중지
         if cur_cash > min_cash:
             try:
-                cur_price = GET_CUR_PRICE(coin) 
-
-                ### 현재 코인의 가격이 0원인 경우 업비트와의 통신에 문제가 있다고 판단 하여 10초뒤 재실행
-                if cur_price == 0:
-                    log("DG","Current Price small than $1.")
-                    cur_price = 1
-                    if buy_price > 1:
-                        before_buy_price = buy_price
-                    buy_price = 0
-                    time.sleep(10)
-                    continue
-
-                if buy_price == 0:
-                    buy_price = before_buy_price
-                    #buy_price = cur_price - (one_tick * 3)
-
-                cur_cash = GET_CASH(coin)
-                if cur_cash < min_cash:     # 보유 현금액이 min_cash 미만이 되면 일괄매도 진행
-                    log("DG","Cash on hand is too low.")
-                    continue
-
-                cur_coin = GET_QUAN_COIN(coin) 
-                if cur_coin * cur_price <= one_tick:
-                    sell_price = 0.0
-
                 wallet = round(cur_cash + (cur_coin * cur_price))
                 log("DG","WALLET : " + str(wallet) , "ACCOUNT : " + str(round(cur_cash)),"COIN_" + str(coin) + " : " + str(cur_coin))
 
@@ -378,13 +385,12 @@ def run(chk_run):
                     rise_chk = 1
 
                 if rise_chk == 1:
-                    one_tick = calculate_tick_unit(cur_price)
                     buy_price = cur_price - (one_tick * 3)
+                    log("INFO","One Tick : " + str(one_tick),"Cur Price : " + str(cur_price),"Buy Price : "+str(buy_price))
                     rise_chk = 0    
 
                 log("DG","CUR_PRICE : " + str(cur_price), "BUY_PRICE  : " + str(buy_price))
                 if cur_price < buy_price:   # 가격이 지정한 매수가 밑으로 내려오면 매수 진행
-                    one_tick = calculate_tick_unit(cur_price)
                     if chk_15m_timer > 3:   # 15분 동안 매수 3회 제한
                         buy_price = cur_price - (one_tick * 3)
                         log("DG","Purchased more than 3 times in 15 minutes.")
@@ -394,10 +400,10 @@ def run(chk_run):
 
                     if res != 0:
                         chk_15m_timer += 1  # 매수 체결시 체크타이머 +1 (15분 마다 0으로 초기화)
+                        log("INFO","Check Timer : " + str(chk_15m_timer))
                         buy_price = cur_price - (one_tick * 3)
                         sell_price = round(GET_BUY_AVG(coin) * 1.03)    # 평균 매수가 +n% 가격을 매도가로 설정
-                        log("DG", "BUY " + str(coin)+ " : " + str(cur_price), "AMOUNT : " + str(buy_amount))
-                        log("DG","Check 15 Minute Timer : " + str(chk_15m_timer))
+                        log("DG", "BUY ORDER " + str(coin)+ " : " + str(cur_price), "AMOUNT : " + str(buy_amount))
 
                 log("DG","CUR_PRICE : " + str(cur_price), "SELL_PRICE : " + str(sell_price))
                 if cur_price >= sell_price and (cur_coin * cur_price) > 6000:   # 가격이 매도가보다 높고, 매도 금액이 최소 주문 단위인 6000원 초과인 경우 매도 진행
@@ -406,62 +412,72 @@ def run(chk_run):
 
                     if res != 0:
                         sell_amount = res['executed_volume']
-                        log("DG", "SELL " + str(coin) + " : " + str(sell_price), "AMOUNT : " + str(sell_amount))
+                        log("DG", "SELL ORDER " + str(coin) + " : " + str(sell_price), "AMOUNT : " + str(sell_amount))
                         sell_price = 0.0
 
                 margin = round(wallet-start_money)  # 수익금
                 margins = round((margin/start_money)*100,2) # 수익률
                 log("DG", "Initial Money : " + str(start_money), "Margin : " + str(margin) + " ("+ str(margins)+" %)")
             except Exception as e:
-                log("DG", "Fail",e)
+                log("DG", "Margin CALC Fail",e)
 
-        else:   # 매도가 이뤄지지 않아 보유 현금액의 n%가 되었을 때 (평단가*1.02)값으로 매도주문 후 대기
+        else:   # 매도가 이뤄지지 않아 보유 현금액의 n%가 되었을 때 (평단가*k%)값으로 매도주문 후 대기
             try:
-                last_order_res = ORDER_SELL_LIMIT(coin)
-                log("DG", "result", last_order_res)
-                if last_order_res == 1: 
-                    order_info = GET_ORDER_INFO(coin).split('&') 
-                    log("DG", "Last Order Success ")
+                order_info = GET_ORDER_INFO(coin)
+                if order_info == 2: # 조회는 성공했으나 주문이 없는 경우
+                    last_order_res = ORDER_SELL_LIMIT(coin,profitPer)
+                    log("INFO", "Last Sell Order Result" + str(last_order_res))
+                elif order_info == 0:
+                    log("DG", "Fail to GET ORDER INFO")
+                    time.sleep(5)
+                    continue
+                else:
+                    order_info = GET_ORDER_INFO(coin).split('&')
+                    last_order_res = 1
+                
+                if last_order_res == 1:     
+                    log("DG", "Last Order Success ", last_order_res)
                     log("DG", "UUID   : "+order_info[0])
                     log("DG", "PRICE  : "+order_info[2])
                     log("DG", "VOLUME : "+order_info[3])
                     chk_sell_order = 1  # 매도가 완료될 때 까지 대기하는 반복문을 실행
+                    time.sleep(10)
                 else:
-                    log("DG", "Fail", last_order_res)
+                    log("DG", "Last Order Fail", last_order_res)
                     continue
                 
             except Exception as e:
                 log("DG", "Fail ", e)
 
+
         while chk_sell_order == 1:
             try:
                 tmp = GET_ORDER_INFO(coin)
-                if tmp != 0:
-                    order_info = tmp.split('&')     # 주문 정보 파싱
+                # 조회는 성공했으나 주문 정보가 없는 경우
+                if tmp == 2:
+                    chk_sell_order = 0
+                    break
+                # 에러
                 elif tmp == 0:
-                    log("DG", "Fail to Get Order Info..")
+                    log("DG", "Fail","GET ORDER INFO Return")
+                    chk_sell_order = 0
                     time.sleep(10)
                     continue
-                else:   # 예외 상황 방생시 코드 종료
-                    log("DG", "Fail","GET_ORDER_INFO Return : " + str(tmp))
-                    chk_run = 2
-                    chk_sell_order = 0
-                    time.sleep(1)
-                    continue
+                else:
+                    order_info = tmp.split('&')     # 주문 정보 파싱
+
                 order_uuid = order_info[0]
                 order_status = GET_ORDER_STATE(order_uuid)
 
                 if order_status == 'wait':
+                    log("DG", "Cur Price :" + GET_CUR_PRICE(coin), "Sell price : " + order_info[2])
                     log("DG", "Sell Order Status : " + order_status)
-                if order_status == 'done':
-                    log("DG", "Sell Order Status : " + order_status)
-                    chk_run = 0 # 매도 주문이 완료되면 다시 메인 코드 진행
-                    chk_sell_order = 0
                 else:
                     log("DG", "Sell Order Status : " + order_status)
                     chk_run = 0
                     chk_sell_order = 0
                 time.sleep(60)
+
             except Exception as e:
                 log("DG", "Fail",e)
                 chk_run = 2
